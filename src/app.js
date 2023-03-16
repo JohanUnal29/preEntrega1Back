6d012cf08913aea8f4f94360e7dca4668cd9846d
Nicolás Ardila <johan.ardilah@gmail.com>
2023-03-15 23:52:36 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const productsRouter = require("./routes/products.router");
+
+
+// Middleware para parsear el body de las requests
+app.use(express.json());
+
+
+// Montamos los routers en sus respectivos paths
+app.use('/api/products', productsRouter);
+//app.use('/api/carts', cartsRouter);
+
+// Iniciamos el servidor
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
